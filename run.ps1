da320f63984f4a21f1786cbc4ee85186ead40649
Nikolche Kolev <nikolev@microsoft.com>
2022-04-05 16:22:26 -0700

    cleanup

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
-.\nuget-6.2.0.exe restore
-dotnet list package --include-transitive
+.\nuget-6.2.0.exe restore NU1701Transitive.sln
+dotnet list package --include-transitive NU1701Transitive.sln
 git clean -xdf
-.\nuget-6.1.0.exe restore
-dotnet list package --include-transitive
+.\nuget-6.1.0.exe restore NU1701Transitive.sln
+dotnet list package --include-transitive NU1701Transitive.sln
